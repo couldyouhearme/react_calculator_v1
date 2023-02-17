eecb69b1b77e586685808c8a86c59a423ef2cf23
Vivian Wu <vivian2021.ai@gmail.com>
2023-02-17 14:47:21 -0800

    add bare bone react calculator_v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [firstInput, setfirstInput] = useState('')
+  const [secondInput, setsecondInput] = useState('')
+  const [res, setRes] = useState('')
+  const [calDisabled, setCalDisabled] = useState(true)
+
+  const handleCalculate = (e) => {
+    switch (e.target.value) {
+      case '1':
+        setRes(parseInt(firstInput) + parseInt(secondInput))
+        break
+      case '2':
+        setRes(parseInt(firstInput) - parseInt(secondInput))
+        break
+      case '3':
+        setRes(parseInt(firstInput) * parseInt(secondInput))
+        break
+      case '4':
+        setRes(parseInt(firstInput) / parseInt(secondInput))
+        break
+      default:
+        console.log('Error!')
+        setRes('')
+    }
+  }
+
+  const handlClear = () => {
+    setfirstInput('')
+    setsecondInput('')
+    setRes('')
+  }
+
+  // Disable numeric operators if two input are not filled
+  useEffect(() => {
+    (firstInput && secondInput) ? setCalDisabled(false) : setCalDisabled(true)
+  }, [firstInput, secondInput])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='firstInput'>
+        <label>
+          Number 1:
+          <input type='test' value={firstInput} onChange={e => setfirstInput(e.target.value)} />
+        </label>
+      </div>
+      <div className='secondInput'>
+        <label>
+          Number 2:
+          <input type='test' value={secondInput} onChange={e => setsecondInput(e.target.value)} />
+        </label>
+      </div>
+      <div className='operator'>
+        <button value={1} onClick={e => handleCalculate(e)} disabled={calDisabled} >+</button>
+        <button value={2} onClick={e => handleCalculate(e)} disabled={calDisabled}>-</button>
+        <button value={3} onClick={e => handleCalculate(e)} disabled={calDisabled}>*</button>
+        <button value={4} onClick={e => handleCalculate(e)} disabled={calDisabled}>/</button>
+        <button onClick={handlClear}>Clear</button>
+      </div>
+      <div className='displayResult'>
+        <input value={res} placeholder={'Your result is...'} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
